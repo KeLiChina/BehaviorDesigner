96d74e7949ac4c1128ae64a2bf857efe53a12c84
ke li <forestlii@outlook.com>
2017-08-24 10:21:22 +0800

    Finish18

---
+++ Assets/Scripts/Task/IsHaveFlag.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+
+public class IsHaveFlag : Conditional {
+   
+    private Offense offense;
+	public override void OnAwake()
+	{
+		offense = this.GetComponent<Offense>();
+	}
+	public override TaskStatus OnUpdate()
+	{
+		if (offense.hasflag){
+			return TaskStatus.Success;
+		}
+		return TaskStatus.Failure;	
+	}
+}
